65fd13708c9ed18f28255ad12c411149c9d40c38
Affan Saleem <m.affansalim@gmail.com>
2026-06-28 11:49:18 +0500

    Update api_config.dart

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -1,7 +1,8 @@
 class ApiConfig {
   // static const String baseUrl = 'http://localhost:3000';
-  // static const String baseUrl = 'https://digift-hr-system-backend-48wi.onrender.com';
-  static const String baseUrl = 'http://145.241.105.37';
+  static const String baseUrl =
+      'https://digift-hr-system-backend-48wi.onrender.com';
+  // static const String baseUrl = 'http://145.241.105.37';
   static const Duration connectTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
 }
